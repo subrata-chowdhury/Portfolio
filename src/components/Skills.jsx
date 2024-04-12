58ce5c1750c598555508170567268804a2e181f3
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-04-12 11:14:51 +0530

    added some more skills and changed skill container style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,59 +15,138 @@ export function SkillsContainer({ forwardSkillContainerRef = useRef(), excludeId
     const skillsData = [{
         name: "Frontend Development",
         iconSrc: "../icons/Frontend2.svg",
-        id: "frontend-development"
+        id: "frontend-development",
+        lvl: 3
     }, {
-        name: "Backend Development",
+        name: "Web Development",
         iconSrc: "../icons/Web Development.svg",
-        id: "web-development"
+        id: "web-development",
+        lvl: 3
     }, {
         name: "HTML",
         iconSrc: "../icons/html.svg",
-        id: "html"
+        id: "html",
+        lvl: 3
     }, {
         name: "CSS",
         iconSrc: "../icons/css.svg",
-        id: "css"
+        id: "css",
+        lvl: 3
     }, {
         name: "SASS",
         iconSrc: "../icons/sass.svg",
-        id: "scaa"
+        id: "scaa",
+        lvl: 3
     }, {
         name: "Java Script",
         iconSrc: "../icons/js.svg",
-        id: "js"
+        id: "js",
+        lvl: 3
     }, {
         name: "PHP",
         iconSrc: "../icons/php.svg",
-        id: "php"
+        id: "php",
+        lvl: 3
     }, {
         name: "C",
         iconSrc: "../icons/c.svg",
-        id: "c"
+        id: "c",
+        lvl: 3
     }, {
         name: "C++",
         iconSrc: "../icons/cpp.svg",
-        id: "cpp"
+        id: "cpp",
+        lvl: 3
     }, {
         name: "Java",
         iconSrc: "../icons/java.svg",
-        id: "java"
+        id: "java",
+        lvl: 3
     }, {
         name: "Python",
         iconSrc: "../icons/python.svg",
-        id: "python"
+        id: "python",
+        lvl: 3
     }, {
         name: "MySQL",
         iconSrc: "../icons/MySQL.svg",
-        id: "mysql"
+        id: "mysql",
+        lvl: 3
     }, {
         name: "NodeJS",
         iconSrc: "../icons/NodeJS.svg",
-        id: "nodejs"
+        id: "nodejs",
+        lvl: 3
     }, {
         name: "React",
         iconSrc: "../icons/React.svg",
-        id: "react"
+        id: "react",
+        lvl: 3
+    }, {
+        name: "Teamwork",
+        iconSrc: "../icons/teamwork.svg",
+        id: "teamwork",
+        lvl: 3
+    }, {
+        name: "Web Design",
+        iconSrc: "../icons/web-design.png",
+        id: "webDesign",
+        lvl: 3
+    }, {
+        name: "Graphic Design",
+        iconSrc: "../icons/graphic-design.png",
+        id: "graphicDesign",
+        lvl: 3
+    }, {
+        name: "AI Tools",
+        iconSrc: "../icons/ai.png",
+        id: "ai",
+        lvl: 3
+    }, {
+        name: "Database",
+        iconSrc: "../icons/database.png",
+        id: "database",
+        lvl: 1
+    }, {
+        name: "Responsive Web Design",
+        iconSrc: "../icons/ui-design.png",
+        id: "responsiveWebDesign",
+        lvl: 2
+    }, {
+        name: "Quick Learner",
+        iconSrc: "../icons/laptop.png",
+        id: "quickLearner",
+        lvl: 2
+    }, {
+        name: "REST API",
+        iconSrc: "../icons/api.png",
+        id: "restApi",
+        lvl: 2
+    }, {
+        name: "Visual Basic",
+        iconSrc: "../icons/vb-file.png",
+        id: "visualBasic",
+        lvl: 2
+    }, {
+        name: "Word",
+        iconSrc: "../icons/word.png",
+        id: "word",
+        lvl: 2
+    }, {
+        name: "Excel",
+        iconSrc: "../icons/excel.png",
+        id: "excel",
+        lvl: 2
+    }, {
+        name: "Power Point",
+        iconSrc: "../icons/powerpoint.png",
+        id: "powerpoint",
+        lvl: 2
+    }, {
+        name: "Power Bi",
+        iconSrc: "../icons/power-bi-2021.svg",
+        id: "powerBi",
+        lvl: 2
     }];
     for (let index = 0; index < skillsData.length; index++) {
         skills.push(<Skill
@@ -77,6 +156,7 @@ export function SkillsContainer({ forwardSkillContainerRef = useRef(), excludeId
             data={excludeIds ? skillsData[index]["id"] : ""}
             key={skillsData[index]["id"]}
             onClickHandler={skillClickHandler}
+            lvl={skillsData[index].lvl}
         />)
     }
     return (
@@ -86,14 +166,18 @@ export function SkillsContainer({ forwardSkillContainerRef = useRef(), excludeId
     )
 }
 
-function Skill({ name = "Skill", icon = "../icons/js.svg", id = "", data = "", onClickHandler = () => { } }) {
+function Skill({ name = "Skill", icon = "../icons/js.svg", id = "", data = "", onClickHandler = () => { }, lvl = 0 }) {
     return (
         <div className="skill-container" id={id} data-id={data} onClick={onClickHandler}>
             <div className="skill-name-container">
                 <img src={icon} alt="icon" />
                 <div className="skill-name">{name}</div>
             </div>
-            <div className="skill-details"></div>
+            <div className="skill-details">
+                <img src={`../icons/star${(lvl === 1 || lvl === 2 || lvl === 3) ? '-fill' : ''}.svg`} alt="icon" />
+                <img src={`../icons/star${(lvl === 2 || lvl === 3) ? '-fill' : ''}.svg`} alt="icon" />
+                <img src={`../icons/star${(lvl === 3) ? '-fill' : ''}.svg`} alt="icon" />
+            </div>
         </div>
     )
 }
