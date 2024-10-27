cb5c4d3f5c593f43d88dd713544acffb3c404f77
Super7000 <subratachowdhurysuper@gmail.com>
2024-10-27 13:33:24 +0530

    rename file names of components for better readability

---
+++ app/layout.jsx
@@ -0,0 +1,40 @@
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="UTF-8" />
+                {/* <link rel="icon" type="image/png" href="/logo.png" /> */}
+                {/* <link href='https://fonts.googleapis.com/css?family=IBM Plex Sans' rel='stylesheet' />
+        <link rel="preload"
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+          rel="stylesheet" crossorigin=""/>
+        <script src="https://cdn.jsdelivr.net/npm/kute.js@2.2.4/dist/kute.min.js"></script> */}
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Subrata Chowdhury | Full-Stack Web Developer Portfolio</title>
+                <meta name="description"
+                    content="Welcome to the professional portfolio of Subrata Chowdhury, showcasing expertise in full-stack web development, React.js, Next.js, and AWS. Explore innovative projects and technical skills." />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:image" content="https://example.com/assets/images/twitter-portfolio.jpg" />
+                <meta property="twitter:title" content="Subrata Chowdhury | Full-Stack Web Developer Portfolio" />
+                <meta property="twitter:description"
+                    content="Explore the professional portfolio of Subrata Chowdhury, featuring projects and skills in full-stack development, React.js, and AWS." />
+
+                <meta property="og:image" content="https://example.com/assets/images/og-portfolio.jpg" />
+                <meta property="og:site_name" content="Subrata Chowdhury Portfolio" />
+                <meta property="og:title" content="Subrata Chowdhury | Full-Stack Web Developer Portfolio" />
+                <meta property="og:description"
+                    content="Discover the portfolio of Subrata Chowdhury, a skilled web developer with expertise in React.js, Next.js, and AWS. Showcasing professional projects and development skills." />
+                <meta property="og:url" content="https://subratachowdhuryportfolio.vercel.app/" />
+
+                <meta name="twitter:creator" content="@Subrata70000" />
+                <meta property="og:type" content="website" />
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
