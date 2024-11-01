3031fe214564f34358a0f048b48f9c87230ea8b5
Super7000 <subratachowdhurysuper@gmail.com>
2024-11-01 19:12:24 +0530

    added a .env file to store config details of firebase

--- app/lib/firebaseInitilizer.js
+++ app/lib/firebaseInitilizer.js
@@ -4,15 +4,17 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://support.google.com/firebase/answer/7015592
 const firebaseConfig = {
-    apiKey: "AIzaSyAq97yIGvx3-sTPA0KQuuNeUwTPJ_Jmj6w",
-    authDomain: "contact-details-48503.firebaseapp.com",
-    projectId: "contact-details-48503",
-    storageBucket: "contact-details-48503.appspot.com",
-    messagingSenderId: "209582268057",
-    appId: "1:209582268057:web:189de925f8b7348526e208",
-    measurementId: "G-14NPTG4M3D"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 // Initialize Firebase
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
